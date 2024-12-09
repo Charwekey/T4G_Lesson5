6af9377fd8a895cfae22f7d9ef4d7d2b7fb7ffc7
Charwekey <charwekeyrabisabutey@gmail.com>
2024-12-10 00:52:19 +1200

    Added all files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 // Event AND event Listening
-let button = document.querySelector("button");
-function changeButton()
-{
-    button.style.backgroundColor="green";
-    button.style.color="white";
-    button.innerHTML="Thanks for submitting you response";
-}
+// let button = document.querySelector("button");
+// function changeButton()
+// {
+//     button.style.backgroundColor="green";
+//     button.style.color="white";
+//     button.innerHTML="Thanks for submitting you response";
+// }
 
-button.addEventListener("click", changeButton);
+// button.addEventListener("click", changeButton);
 
 // changeButton
+
+let button= document.querySelector("button");
+function openImage(){
+ let image = document.querySelector(".img")
+ image.style.display="block"
+ let close = document.querySelector(".close")
+ close.style.display ="block"
+}
+button.addEventListener("click" ,openImage)
+
+let close= document.querySelector(".close");
+function closeImage(){
+ let image = document.querySelector(".img")
+ image.style.display="none"
+//  let button = document.querySelector("button")
+//  close.style.display ="bl"
+}
+close.addEventListener("click" ,closeImage)
